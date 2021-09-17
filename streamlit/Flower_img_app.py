e86b6e7bd283ffee6305ca55fbcd156c17fbce25
Kabilan-n <60337704+Kabilan-n@users.noreply.github.com>
2021-09-17 10:31:39 +0530

    Update Flower_img_app.py

--- streamlit/Flower_img_app.py
+++ streamlit/Flower_img_app.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import os
-eff_model = tf.keras.load_model(os.path.join("./model/","eff_model.hdf5"))
+eff_model = tf.keras.models.load_model(os.path.join("./model/","eff_model.hdf5"))
 #eff_model = tf.keras.models.load_model('model/eff_model.hdf5',compile=False)
 MobileNet_model = tf.keras.models.load_model('model/MobileNet_model.hdf5',compile=False)
 #res_model =  tf.keras.models.load_model('model\Res_model.h5',compile=False)
